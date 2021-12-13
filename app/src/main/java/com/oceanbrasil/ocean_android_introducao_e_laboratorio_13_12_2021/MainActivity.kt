c81d5f56013410edc36da645b7064a7128cf38f3
Paulo Salvatore <salvatoregames@gmail.com>
2021-12-13 11:51:05 -0300

    Atualizando o texto do `tvResultado` quando clica no `btEnviar`

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_e_laboratorio_13_12_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_e_laboratorio_13_12_2021/MainActivity.kt
@@ -2,10 +2,24 @@ package com.oceanbrasil.ocean_android_introducao_e_laboratorio_13_12_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Acessar o elemento que está no layout XML
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+
+        // Declaramos um ouvinte para eventos de clique no btEnviar
+        // Assim que um clique no btEnviar ocorrer, o código dentro
+        // das chaves { } será executado
+        btEnviar.setOnClickListener {
+            // Alteramos o texto do tvResultado para uma string que quisermos
+            tvResultado.text = "Paulo Salvatore"
+        }
     }
 }
