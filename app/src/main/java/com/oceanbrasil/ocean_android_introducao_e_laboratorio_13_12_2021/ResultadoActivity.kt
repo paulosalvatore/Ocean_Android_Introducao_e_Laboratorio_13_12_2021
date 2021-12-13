a563fd54b8691260118b27e6ae07ebb30be3bdbc
Paulo Salvatore <salvatoregames@gmail.com>
2021-12-13 14:52:37 -0300

    Criação da tela ResultadoActivity`

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_e_laboratorio_13_12_2021/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.ocean_android_introducao_e_laboratorio_13_12_2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
