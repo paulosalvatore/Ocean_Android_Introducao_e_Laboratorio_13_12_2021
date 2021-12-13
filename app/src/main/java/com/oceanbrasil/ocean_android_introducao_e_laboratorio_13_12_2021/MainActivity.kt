6e5c9de403a86045e91c7e8a5f642ad4f3c96979
Paulo Salvatore <salvatoregames@gmail.com>
2021-12-13 14:38:21 -0300

    Atualização do `TextView` baseado no que foi digitado no `EditText`

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_e_laboratorio_13_12_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_e_laboratorio_13_12_2021/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_android_introducao_e_laboratorio_13_12_2021
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +14,18 @@ class MainActivity : AppCompatActivity() {
         // Acessar o elemento que está no layout XML
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val etNome = findViewById<EditText>(R.id.etNome)
 
         // Declaramos um ouvinte para eventos de clique no btEnviar
         // Assim que um clique no btEnviar ocorrer, o código dentro
         // das chaves { } será executado
         btEnviar.setOnClickListener {
-            // Alteramos o texto do tvResultado para uma string que quisermos
-            tvResultado.text = "Paulo Salvatore"
+            // Pegamos o que foi digitado no EditText
+            val nome = etNome.text
+
+            // Alteramos o texto do tvResultado para o valor que está armazenado na
+            // variável 'nome'
+            tvResultado.text = nome
         }
     }
 }
